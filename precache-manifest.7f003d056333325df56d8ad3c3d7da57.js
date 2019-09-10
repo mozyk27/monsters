cf7b3208205380896ab80c47040244c591270939
Kung, Jimmy <jimkung@netsuite.com>
2019-09-09 22:12:38 -0500

    Updates

---
+++ precache-manifest.7f003d056333325df56d8ad3c3d7da57.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ca42c9409c2624398c2cf604e6868d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "444489b784bfa5f98049",
+    "url": "/monsters/static/css/main.6f6d2e2f.chunk.css"
+  },
+  {
+    "revision": "e260c628452a8e3fa795",
+    "url": "/monsters/static/js/2.cdcd8c14.chunk.js"
+  },
+  {
+    "revision": "444489b784bfa5f98049",
+    "url": "/monsters/static/js/main.4b70750e.chunk.js"
+  },
+  {
+    "revision": "083ca4195e7c022820b5",
+    "url": "/monsters/static/js/runtime~main.09c3ff9b.js"
+  }
+]);
